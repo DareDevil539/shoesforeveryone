6bcbeaa3402c8040bffa6452ce85604742f3e88c
daredevil539 <olenishak@gmail.com>
2018-07-20 21:55:40 +0300

    included config.php and db.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php require 'includes/config.php'; require 'includes/db.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>
+    <?php echo $config['title']; ?>
+  </title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  
+</body>
+
+</html>
