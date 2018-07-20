1032649d7258ff53e3722618aef1b3efe44d5ab9
daredevil539 <olenishak@gmail.com>
2018-07-20 21:57:14 +0300

    creaetd and set up the config for admin

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = array(
+  'title' => 'Shoes For Everyone!',
+  'db' => array(
+    'server' => 'sql110.epizy.com',
+    'username' => 'epiz_22429273',
+    'password' => 'vebmYaQ00NdQzwn',
+    'dbName' => 'epiz_22429273_shoesforeveryone'
+  )
+);
