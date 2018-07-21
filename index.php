fc0e34fd16752bb32de05d0782c6ecc809813591
daredevil539 <olenishak@gmail.com>
2018-07-21 11:27:26 +0300

    rewrited index.php; added main enter point

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
-<?php require 'includes/config.php'; require 'includes/db.php'; ?>
+<?php
+require 'includes/config.php';
 
-<!DOCTYPE html>
-<html lang="en">
+if ($_SERVER['REDIRECT_URL']=='/') {
+    $page='home';
+} else {
+    $page=substr($_SERVER['REDIRECT_URL'], 1);
+    if (!preg_match('/^[A-z0-9]{3,15}$/', $page)) {
+        exit('error url');
+    }
+}
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>
-    <?php echo $config['title']; ?>
-  </title>
-  <link rel="stylesheet" href="css/style.css">
-</head>
+session_start();
 
-<body>
-  
-</body>
-
-</html>
+if (file_exists('pages/' . $page . '.php')) {
+    include 'pages/' . $page . '.php';
+} else {
+    include 'pages/errors/404.php';
+    exit();
+}
+?>﻿
