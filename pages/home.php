5c4b86d14af338ca5c128bd5b28a28fd81afd2b5
daredevil539 <olenishak@gmail.com>
2018-07-21 12:44:05 +0300

    added basic html template with help of wrapper

--- pages/home.php
+++ pages/home.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title><?php echo $config['title']; ?></title>
-</head>
-<body>
+<?php wrapper_top($config['title']); ?>
 
-</body>
-</html>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-12">serus</div>
+  </div>
+</div>
+
+<?php wrapper_bottom(); ?>
