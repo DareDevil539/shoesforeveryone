502c3edccdb08a728edc22820122fff616c7e41a
daredevil539 <olenishak@gmail.com>
2018-07-21 17:17:23 +0300

    added favicon

--- pages/home.php
+++ pages/home.php
@@ -2,7 +2,14 @@
 
 <div class="container-fluid">
   <div class="row">
-    <div class="col-lg-12">serus</div>
+    <div class="header col-12">
+      <div class="logo col-3"><img src="img/logo.svg" alt="Logo" height="100px"></div>
+      <div class="menu col-5 offset-4">
+        <div class="menu-itm col"><a href="#">home</a></div>
+        <div class="menu-itm col offset-1"><a href="#">about</a></div>
+        <div class="menu-itm col offset-1"><a href="#">contact us</a></div>
+      </div>
+    </div>
   </div>
 </div>
 
