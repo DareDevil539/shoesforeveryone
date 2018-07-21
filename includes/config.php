eb5c791b66230f199f4b30109c64e4cb08f11d01
daredevil539 <olenishak@gmail.com>
2018-07-21 17:16:36 +0300

    updated config for new domain name

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,11 @@
 <?php
 
 $config = array(
-  'title' => 'Shoes For Everyone!',
+  'title' => 'TENTech. Devices for everyone',
   'db' => array(
     'server' => 'sql110.epizy.com',
     'username' => 'epiz_22429273',
     'password' => 'vebmYaQ00NdQzwn',
-    'dbName' => 'epiz_22429273_shoesforeveryone'
+    'dbName' => 'epiz_22429273_tentech'
   )
 );
