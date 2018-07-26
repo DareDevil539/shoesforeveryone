bfbded65858b9bef2f892cd90ce3217919e17b36
daredevil539 <olenishak@gmail.com>
2018-07-26 18:17:24 +0300

    fixed html structure

--- includes/main.php
+++ includes/main.php
@@ -7,12 +7,12 @@
         <div class="flipcard">
           <div class="front">
             <div class="top-img" style="background-image: url(/img/<?php echo $article['image']; ?>)"></div>
-            <h4 class="flip-title"><?php echo mb_substr($article['type'] . ' ' . $article['brand'] . ' ' . $article['model'], 0, 30) . '...'; ?></h5>
+            <h4 class="flip-title"><?php echo mb_substr($article['type'] . ' ' . $article['brand'] . ' ' . $article['model'], 0, 30) . '...'; ?></h4>
               <div class="alert alert-primary" role="alert"><?php echo '$' . $article['price']; ?></div>
             </div>
             <div class="back">
               <div class="top-img" style="background-image: url(/img/<?php echo $article['image']; ?>)"></div>
-              <h4 class="flip-title"> <?php echo mb_substr($article['type'] . ' ' . $article['brand'] . ' ' . $article['model'], 0, 30) . '...'; ?></h5>
+              <h4 class="flip-title"> <?php echo mb_substr($article['type'] . ' ' . $article['brand'] . ' ' . $article['model'], 0, 30) . '...'; ?></h4>
               <p><?php echo mb_substr($article['description'], 0, 150) . '...'; ?></p>
               <div class="alert alert-primary" role="alert"><?php echo '$' . $article['price']; ?></div>
               <a href="/articles?id=<?php echo $article['id']; ?>" class="btn btn-primary" target="_blank">Details</a>
