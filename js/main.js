86a639b2b7c4fe1e97695ceaad50391838a1bb5a
daredevil539 <olenishak@gmail.com>
2018-07-26 18:18:31 +0300

    added burger-button

--- js/main.js
+++ js/main.js
@@ -1,22 +1,31 @@
 $("document").ready(function() {
-  $(".t-but").click(function() {
-    $(".menu-bar").toggleClass("menu-bar-slide");
-    $(".header-hidden").toggleClass("header-slide");
+  $(".t-but").click(function(event) {
+    event.preventDefault;
+    $(this).toggleClass("t-but-active");
+    if ($(this).hasClass("t-but-active")) {
+      $(".header-hidden").css("height", "200px");
+      $(".menu-hidden, .m-h-item, .m-h-item a").css("opacity", "1");
+      $(".menu-bar").css("margin-top", "200px");
+    } else {
+      $(".header-hidden").css("height", "0");
+      $(".menu-hidden, .m-h-item, .m-h-item a").css("opacity", "0");
+      $(".menu-bar").css("margin-top", "0");
+    }
   });
 
   $(".menu .menu-itm").mouseover(function() {
-    $(this).children().css("color", "#333")
+    $(this).children().css("color", "#212121")
   });
 
   $(".menu .menu-itm").mouseout(function() {
-    $(this).children().css("color", "#efefef")
+    $(this).children().css("color", "#fff")
   });
 
   $(".menu-hidden .m-h-item").mouseover(function() {
-    $(this).children().css("color", "#333")
+    $(this).children().css("color", "#212121")
   });
 
   $(".menu-hidden .m-h-item").mouseout(function() {
-    $(this).children().css("color", "#efefef")
+    $(this).children().css("color", "#fff")
   });
 });
