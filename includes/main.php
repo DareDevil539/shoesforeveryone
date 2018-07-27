bc704149c0b2a0a57d4ba5246b0578fdb4fa3303
daredevil539 <olenishak@gmail.com>
2018-07-27 09:40:32 +0300

    fixed the card`s html

--- includes/main.php
+++ includes/main.php
@@ -4,22 +4,26 @@
       <?php while ($article = mysqli_fetch_assoc($articles)) {
     ?>
       <div class="col-wr">
-        <div class="flipcard">
-          <div class="front">
-            <div class="top-img" style="background-image: url(/img/<?php echo $article['image']; ?>)"></div>
-            <h4 class="flip-title"><?php echo mb_substr($article['type'] . ' ' . $article['brand'] . ' ' . $article['model'], 0, 30) . '...'; ?></h4>
-              <div class="alert alert-primary" role="alert"><?php echo '$' . $article['price']; ?></div>
+        <div class="anCard">
+          <div class="top-img" style="background-image: url(/img/<?php echo $article['image']; ?>)"></div>
+
+          <div class="card-body">
+            <div class="card-title">
+              <h4 title="<?php echo $article['type'] . ' ' . $article['brand'] . ' ' . $article['model']; ?>"> <?php echo mb_substr($article['type'] . ' ' . $article['brand'] . ' ' . $article['model'], 0, 20) . '...'; ?></h4>
             </div>
-            <div class="back">
-              <div class="top-img" style="background-image: url(/img/<?php echo $article['image']; ?>)"></div>
-              <h4 class="flip-title"> <?php echo mb_substr($article['type'] . ' ' . $article['brand'] . ' ' . $article['model'], 0, 30) . '...'; ?></h4>
-              <p><?php echo mb_substr($article['description'], 0, 150) . '...'; ?></p>
-              <div class="alert alert-primary" role="alert"><?php echo '$' . $article['price']; ?></div>
-              <a href="/articles?id=<?php echo $article['id']; ?>" class="btn btn-primary" target="_blank">Details</a>
+            <div class="card-price">
+              <?php echo '$' . $article['price']; ?>
             </div>
+            <div class="card-desc">
+              <p>
+                <?php echo mb_substr($article['description'], 0, 150) . '...'; ?>
+              </p>
+            </div>
+            <a href="/articles?id=<?php echo $article['id']; ?>" class="btn btn-primary" target="_blank">Details</a>
           </div>
         </div>
-    <?php
+      </div>
+      <?php
 } ?>
     </div>
   </div>
