e9e377bdfd2a2f1ca792d03801330ce48f018c44
daredevil539 <olenishak@gmail.com>
2018-07-29 20:55:57 +0300

    added login form for admin-panel

---
+++ pages/admin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Log.IN</title>
+  <link rel="stylesheet" href="css/admin.css">
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="wio-login">
+        <img src="../img/admin-logo.svg" alt="Web.IO Admin Panel">
+        <form action="wio_admin" method="post">
+          <label for="username">Login</label>
+          <input type="text" placeholder="Login" id="username" name="username">
+          <label for="password">Password</label>
+          <input type="password" placeholder="Password" id="password" name="password">
+          <input type="submit" value="Log In">
+        </form>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
