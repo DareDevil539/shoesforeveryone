6402f7b06bf246b66b8e2c1fff4f6ecd59ee8af1
daredevil539 <olenishak@gmail.com>
2018-07-29 21:36:55 +0300

    added favicon

--- pages/admin.php
+++ pages/admin.php
@@ -6,7 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Log.IN</title>
-  <link rel="stylesheet" href="css/admin.css">
+  <link rel="shortcut icon" href="/img/admin-favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="/css/admin.css">
 </head>
 
 <body>
